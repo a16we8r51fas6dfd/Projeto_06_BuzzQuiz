186dc1dca450f5ce83c545ced9daab80d7602ae8
fernandinho <fernando_yudi_yaeda@hotmail.com>
2021-11-18 20:21:29 -0300

    3/4 card 1 e layouts card 1 e card 2

---
+++ script.js
@@ -0,0 +1,23 @@
+const criacaoQuizElemento = document.querySelector(".criacao-quiz")
+const criacaoPerguntasElemento = document.querySelector(".criacao-perguntas")
+let informacoesBasicasQuiz = {}
+
+function prosseguirPerguntas() {
+    const infoTitulo = document.querySelector(".info-titulo").value
+    const infoURL = document.querySelector(".info-url").value
+    const infoPerguntas = document.querySelector(".info-qtd-perguntas").value
+    const infoNiveis = document.querySelector(".info-qtd-niveis").value
+
+
+    if ((19 > infoTitulo.length && infoTitulo.length < 65) || infoPerguntas < 3 || infoNiveis < 2) {
+        alert("respondeu o formulario errado doidão, lanse a braba novamente")
+    } else {
+        criacaoQuizElemento.classList.add("escondido")
+        criacaoPerguntasElemento.classList.remove("escondido")
+    }
+}
+
+function prosseguirNiveis() {
+    console.log("eae bobao")
+}
+
